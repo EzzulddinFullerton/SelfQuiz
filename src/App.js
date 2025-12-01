59b5fde55a02b5f1586b5a1d0522db621a3c51be
scorpion t <ezzulddin@csu.fullerton.edu>
2025-11-30 18:45:48 -0800

    Github pages and deploy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Quiz from './pages/Quiz';
+import ScoreSummary from './pages/ScoreSummary';
+import HighScores from './pages/HighScores';
+import { HighScoreProvider } from './context/HighScoreContext';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HighScoreProvider>
+      <Router>
+        <div className="min-h-screen bg-gray-100 text-gray-800 font-sans">
+          <Navbar />
+          <div className="container mx-auto p-4">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/quiz" element={<Quiz />} />
+              <Route path="/score" element={<ScoreSummary />} />
+              <Route path="/highscores" element={<HighScores />} />
+            </Routes>
+          </div>
+        </div>
+      </Router>
+    </HighScoreProvider>
   );
 }
 
